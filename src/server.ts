c83972e978765cb58a4b3d5c04195b22431b28d2
nayeem-miah <nayeem5113a@gmail.com>
2025-07-28 11:13:24 +0600

    setup done

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,18 @@
+/* eslint-disable no-console */
 import { Server } from "http";
 import mongoose from "mongoose";
 import app from "./app";
+import { envVars } from "./app/modules/config/env";
 
 let server: Server;
 
 const stateServer = async () => {
     try {
-        await mongoose.connect("mongodb://localhost:27017/tour-management-backend")
+        await mongoose.connect(envVars.DB_URL);
         console.log("Connected to DB!");
 
-        server = app.listen(5000, () => {
-            console.log(`Server is listening to port http://localhost:5000`)
+        server = app.listen(envVars.PORT, () => {
+            console.log(`Server is listening to port http://localhost:${envVars.PORT}`)
         });
     } catch (error) {
         console.log(error);
@@ -18,4 +20,58 @@ const stateServer = async () => {
 };
 stateServer();
 
+process.on("unhandledRejection", (err) => {
+    console.log("unhandled rejection detracted .............Server shutting down", err);
 
+    if (server) {
+        server.close(() => {
+            process.exit(1)
+        })
+    }
+
+    process.exit(1);
+});
+
+process.on("uncaughtException", (err) => {
+    console.log("uncaught exception error........ server is shutting down", err);
+    if (server) {
+        server.close(() => {
+            process.exit(1)
+        })
+    }
+    process.exit(1)
+})
+
+process.on("SIGTERM", () => {
+    console.log("SIGTERM signal received .........server shutting down");
+    if (server) {
+        server.close(() => {
+            process.exit(1)
+        })
+    }
+    process.exit(1)
+})
+
+process.on("SIGINT", () => {
+    console.log("SIGINT signal received .........server shutting down");
+    if (server) {
+        server.close(() => {
+            process.exit(1)
+        })
+    }
+    process.exit(1)
+})
+
+// unhandled rejection error
+// Promise.reject(new Error("i forgot to catch this Promise"));
+
+// uncaught rejection error
+// throw new Error("I forgot to handle this local error");
+
+// server error handling
+/**
+ * unhandled rejection error
+ * uncaught rejection error
+ * signal termination sigterm
+ * 
+ */
