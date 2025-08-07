0d61b76c01e1b18a73f59f1a650ff6c8f42723f8
nayeem-miah <nayeem5113a@gmail.com>
2025-08-07 12:21:04 +0600

    pre hook tour update

--- src/app/modules/tour/tour.model.ts
+++ src/app/modules/tour/tour.model.ts
@@ -53,22 +53,23 @@ const tourSchema = new Schema<ITour>({
 
 
 
+
 // tourSchema.pre("save", async function (next) {
+
 //     if (this.isModified("title")) {
 //         const baseSlug = this.title.toLowerCase().split(" ").join("-")
-//         let slug = `${baseSlug}`;
+//         let slug = `${baseSlug}`
 
-//         let counter = 0
+//         let counter = 0;
 //         while (await Tour.exists({ slug })) {
-//             slug = `${slug}-${counter++}`  // dhaka-1
+//             slug = `${slug}-${counter++}`
 //         }
 
 //         this.slug = slug;
-
 //     }
-
 //     next()
 // })
+
 
 tourSchema.pre("findOneAndUpdate", async function (next) {
     const tour = this.getUpdate() as Partial<ITour>
--- src/app/modules/tour/tour.service.ts
+++ src/app/modules/tour/tour.service.ts
@@ -3,24 +3,26 @@ import { tourSearchableFields } from "./tour.constant";
 import { ITour, ITourType } from "./tour.interface";
 import { Tour, TourType } from "./tour.model";
 
-const createTour = async (payload: Partial<ITour>) => {
-    const existingTour = await Tour.findOne({ title: payload.title });
 
+const createTour = async (payload: ITour) => {
+    const existingTour = await Tour.findOne({ title: payload.title });
     if (existingTour) {
         throw new Error("A tour with this title already exists.");
     }
-    const baseSlug = payload.title?.toLowerCase().split(" ").join("-")
+
+    const baseSlug = payload.title.toLowerCase().split(" ").join("-")
     let slug = `${baseSlug}`
 
     let counter = 0;
     while (await Tour.exists({ slug })) {
-        slug = `${slug}-${counter++}` // dhaka-2
+        slug = `${slug}-${counter++}` // dhaka-division-2
     }
 
     payload.slug = slug;
 
-    const tour = await Tour.create(payload);
-    return tour
+    const tour = await Tour.create(payload)
+
+    return tour;
 };
 
 
