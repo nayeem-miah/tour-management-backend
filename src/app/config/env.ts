82b01b8eec662e509cd8661909b6798e3ec03a3e
nayeem-miah <nayeem5113a@gmail.com>
2025-08-10 06:29:21 +0600

    ssl payment data init

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -24,11 +24,17 @@ interface EnvConfig {
         SSL_STORE_PASS: string,
         SSL_PAYMENT_API: string,
         SSL_VALIDATION_API: string,
+        SSL_SUCCESS_BACKEND_URL: string,
+        SSL_FAIL_BACKEND_URL: string,
+        SSL_CANCEL_BACKEND_URL: string,
+        SSL_SUCCESS_FRONTEND_URL: string,
+        SSL_FAIL_FRONTEND_URL: string,
+        SSL_CANCEL_FRONTEND_URL: string,
     }
 }
 
 const loadEnvVariables = (): EnvConfig => {
-    const requiredEnvVariables: string[] = ["PORT", "DB_URL", "NODE_ENV", "JWT_ACCESS_SECRET", "JWT_ACCESS_EXPIRES", "BCRYPT_SLOT_ROUND", "SUPER_ADMIN_EMAIL", "SUPER_ADMIN_PASSWORD", "JWT_REFRESH_SECRET", "JWT_REFRESH_EXPIRE", "GOOGLE_CLIENT_ID", "GOOGLE_CLIENT_SECRET", "GOOGLE_CALLBACK_URL", "EXPRESS_SESSION", "FRONTEND_URL", "SSL_STORE_ID", "SSL_STORE_PASS", "SSL_PAYMENT_API", "SSL_VALIDATION_API"];
+    const requiredEnvVariables: string[] = ["PORT", "DB_URL", "NODE_ENV", "JWT_ACCESS_SECRET", "JWT_ACCESS_EXPIRES", "BCRYPT_SLOT_ROUND", "SUPER_ADMIN_EMAIL", "SUPER_ADMIN_PASSWORD", "JWT_REFRESH_SECRET", "JWT_REFRESH_EXPIRE", "GOOGLE_CLIENT_ID", "GOOGLE_CLIENT_SECRET", "GOOGLE_CALLBACK_URL", "EXPRESS_SESSION", "FRONTEND_URL", "SSL_STORE_ID", "SSL_STORE_PASS", "SSL_PAYMENT_API", "SSL_VALIDATION_API", "SSL_SUCCESS_BACKEND_URL", "SSL_FAIL_BACKEND_URL", "SSL_CANCEL_BACKEND_URL", "SSL_SUCCESS_FRONTEND_URL", "SSL_FAIL_FRONTEND_URL", "SSL_CANCEL_FRONTEND_URL"];
 
     requiredEnvVariables.forEach(key => {
         if (!process.env[key]) {
@@ -58,6 +64,12 @@ const loadEnvVariables = (): EnvConfig => {
             SSL_STORE_PASS: process.env.SSL_STORE_PASS as string,
             SSL_PAYMENT_API: process.env.FRONTEND_URL as string,
             SSL_VALIDATION_API: process.env.SSL_VALIDATION_API as string,
+            SSL_SUCCESS_BACKEND_URL: process.env.SSL_SUCCESS_BACKEND_URL as string,
+            SSL_FAIL_BACKEND_URL: process.env.SSL_FAIL_BACKEND_URL as string,
+            SSL_CANCEL_BACKEND_URL: process.env.SSL_CANCEL_BACKEND_URL as string,
+            SSL_SUCCESS_FRONTEND_URL: process.env.SSL_SUCCESS_FRONTEND_URL as string,
+            SSL_FAIL_FRONTEND_URL: process.env.SSL_FAIL_FRONTEND_URL as string,
+            SSL_CANCEL_FRONTEND_URL: process.env.SSL_CANCEL_FRONTEND_URL as string,
         }
     }
 };
---
+++ src/app/modules/sslCommerce/sslCommerce.interface.ts
@@ -0,0 +1,8 @@
+export interface ISslCommerce {
+    amount: string;
+    transactionId: string;
+    name: string;
+    email: string;
+    phoneNumber: string;
+    address: string
+}
