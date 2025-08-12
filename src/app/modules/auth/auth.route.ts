f17b5f204f2ba60018e7ec07e09bca1329163257
nayeem-miah <nayeem5113a@gmail.com>
2025-08-12 11:47:02 +0600

    google login and password set

--- src/app/modules/auth/auth.route.ts
+++ src/app/modules/auth/auth.route.ts
@@ -11,6 +11,8 @@ router.post("/login", AuthControllers.credentialsLogin);
 router.post("/refresh-token", AuthControllers.getNewAccessToken);
 router.post("/logout", AuthControllers.logout);
 router.post("/reset-password", checkAuth(...Object.values(Role)), AuthControllers.resetPassword);
+router.post("/change-password", checkAuth(...Object.values(Role)), AuthControllers.changePassword);
+router.post("/set-password", checkAuth(...Object.values(Role)), AuthControllers.setPassword);
 
 // booking ---> /login ----> success login --> /booking frontend
 // /login ----> successfully google login ---> frontend
